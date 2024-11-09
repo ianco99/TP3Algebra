8db474abad263598d38ae7c3a89043f4e8c72153
FrostPowerX <103634826+FrostPowerX@users.noreply.github.com>
2024-11-08 23:47:15 -0300

    remove return and change condition

--- Algebra-TP3/Assets/Scripts/Model.cs
+++ Algebra-TP3/Assets/Scripts/Model.cs
@@ -21,36 +21,36 @@ public class Model : MonoBehaviour
     {
         meshFilter = GetComponentInChildren<MeshFilter>();
 
-        if (meshFilter == null)
-            return;
-
-        Vector3[] vertices = meshFilter.mesh.vertices;
-
-        for (int i = 0; i < meshFilter.mesh.triangles.Length; i += 3) 
-        { 
-            Triangle triangle = new Triangle();
-
-            int vertIndex1 = meshFilter.mesh.triangles[i];
-            int vertIndex2 = meshFilter.mesh.triangles[i + 1];
-            int vertIndex3 = meshFilter.mesh.triangles[i + 2];
-
-            triangle.vertices[0] = vertices[vertIndex1];
-            triangle.vertices[1] = vertices[vertIndex2];
-            triangle.vertices[2] = vertices[vertIndex3];
-
-            triangles.Add(triangle);
-        }
-
-        for (int i = 0; i < triangles.Count; i++) 
+        if (meshFilter != null)
         {
-            Plane plane = new Plane(triangles[i].vertices[0], triangles[i].vertices[1], triangles[i].vertices[2]);
-            planes.Add(plane);
+            Vector3[] vertices = meshFilter.mesh.vertices;
+
+            for (int i = 0; i < meshFilter.mesh.triangles.Length; i += 3)
+            {
+                Triangle triangle = new Triangle();
+
+                int vertIndex1 = meshFilter.mesh.triangles[i];
+                int vertIndex2 = meshFilter.mesh.triangles[i + 1];
+                int vertIndex3 = meshFilter.mesh.triangles[i + 2];
+
+                triangle.vertices[0] = vertices[vertIndex1];
+                triangle.vertices[1] = vertices[vertIndex2];
+                triangle.vertices[2] = vertices[vertIndex3];
+
+                triangles.Add(triangle);
+            }
+
+            for (int i = 0; i < triangles.Count; i++)
+            {
+                Plane plane = new Plane(triangles[i].vertices[0], triangles[i].vertices[1], triangles[i].vertices[2]);
+                planes.Add(plane);
+            }
         }
     }
 
     void OnDrawGizmos()
     {
-        for (int i = 0;i < planes.Count;i++)
+        for (int i = 0; i < planes.Count; i++)
         {
             planes[i].DrawGizmo(transform);
         }
@@ -60,14 +60,14 @@ public class Model : MonoBehaviour
     {
         bool isContain = true;
 
-        for(int i = 0; i < planes.Count; i++)
+        for (int i = 0; i < planes.Count; i++)
         {
             Vector3 posP = planes[i].Center;
             Vector3 normal = planes[i].Normal;
 
             float dot = (point.x - posP.x) * normal.x + (point.y - posP.y) * normal.y + (point.z - posP.z) * normal.z;
 
-            if(dot < 0)
+            if (dot < 0)
             {
                 isContain = false;
                 break;
